f51e05d0491b3fac9d5459b5e849b2b9b6c06553
Marcel Eichner <love@ephigenia.de>
2014-10-06 18:54:26 +0200

    [feature] adds Youtube ChannelInfo Test (#gh-22)
    This adds a test for tracking user (which is a channel) information from youtube. With this you can track the followers and number of views (total) of a single user / channel on youtube.

---
+++ lib/franklin/test/Youtube/ChannelInfo/ChannelInfo.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Franklin\test\Youtube\ChannelInfo;
+
+use
+    Franklin\test\Scrape\Scrape,
+    Franklin\network\CURL
+    ;
+
+class ChannelInfo extends Scrape
+{
+    public $name = 'Youtube Channel Information';
+    
+    public $description = 'Information about a specific youtube user / channel';
+    
+    public function run()
+    {
+        $this->beforeRun();
+
+        $url = sprintf('https://www.youtube.com/user/%s/about', $this->config->username);
+        $CURL = new CURL();
+        $response = $CURL->get($url);
+        if ($result = $this->processResponse($response)) {
+            $result = $this->convertValue($result);
+        }
+        return $result;
+    }
+
+    public function processResponse($response)
+    {
+        if (!$response) {
+            return false;
+        }
+
+        $regexps = array(
+            'followers' => '/([\d\.]+)<\/b> Abonnenten/',
+            'views' => '/([\d\.]+)<\/b> Aufrufe/',
+        );
+
+        // invalid key that should be found
+        if (!isset($regexps[$this->config->key])) {
+            return false;
+        }
+
+        if (preg_match_all($regexps[$this->config->key], $response, $found)) {
+            return $found[1][0];
+        }
+
+        return false;
+    }
+}
